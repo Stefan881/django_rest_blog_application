6d8b3f00b82e5b09d177d0d1668bbea59ec19b70
Ondeyo Stephen <ondeyostephen0@gmail.com>
2024-01-10 13:15:48 +0300

    Registration with POST Request

--- account/serializers.py
+++ account/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class RegisterSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
     username = serializers.CharField()
     password = serializers.CharField()
 
@@ -13,4 +15,14 @@ class RegisterSerializer(serializers.Serializer):
         
 
         return data
+    
+
+    def create(self, validated_data):
+       user = User.objects.create(first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+            username = validated_data['username'].lower()
+           )
+       user.set_password(validated_data['password'])
+
+       return validated_data
         
